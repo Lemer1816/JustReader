67550ebe1a5bdbc59059a93a69eddf413f86529b
Lemonade <842476697@qq.com>
2017-06-26 08:56:48 +0800

    Initial Commit

---
+++ JustReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JustReader
+//
+//  Created by Lemonade on 2017/6/26.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
