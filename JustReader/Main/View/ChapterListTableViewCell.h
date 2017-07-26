b40c3bc32e117342ae514b15dcd1733d2a6dc101
Lemonade <842476697@qq.com>
2017-07-26 17:11:49 +0800

    完成书籍详情页

---
+++ JustReader/Main/View/ChapterListTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ChapterListTableViewCell.h
+//  JustReader
+//
+//  Created by Lemonade on 2017/7/26.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChapterListTableViewCell : UITableViewCell
+
+@end
---
+++ JustReader/Main/View/ChapterListTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ChapterListTableViewCell.m
+//  JustReader
+//
+//  Created by Lemonade on 2017/7/26.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import "ChapterListTableViewCell.h"
+
+@implementation ChapterListTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
